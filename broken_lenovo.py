9e9fab64a01f8ef3ad1cf7cc00b4882fec685181
returncoder2020 <returncoder2020@gmail.com>
2021-03-15 00:50:31 -0400

    4th commit on March 15, 2021

--- broken_lenovo.py
+++ broken_lenovo.py
@@ -1,3 +1,4 @@
 print("Repairing one of my pcs")
 print("Using a Restore Point to get one of my pcs up and running")
 print("Watching a video about Distributed Systems")
+print("Learning about different Queries in some systems")
